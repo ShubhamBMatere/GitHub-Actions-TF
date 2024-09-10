5519f62b45ffa79b72f3dd2bf906cea6421f4033
user1 <shubhambmatere012@gmail.com>
2024-09-10 20:29:18 +0530

    s3 region

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-terraform-state"
-    region         = "us-east-1"
+    region         = "ap-south-2"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
   }
